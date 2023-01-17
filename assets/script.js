7563d323dc980cc742790393406df9ffba016402
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-01-16 19:57:29 -0500

    functions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-var topic = "HTML";
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
 
-if (topic === 'HTML') {
+function selectTopic() {
+  if (topic === 'HTML') {
     console.log("Let's study HTML!");
-}   else if (topic === 'CSS') {
-    console.log ("let's study CSS!");
-}   else if (topic === 'Git') {
-    console.log ("let's study Git!");
-}   else if (topic === 'JavaScript') {
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
     console.log("Let's study JavaScript!");
-}   else {
-    console.log("Plese try again!");
+  } else {
+    console.log('Please try again!');
+  }
 }
-
-var shapes = ["triangle", "square", "pentagon", "circle"];
-for(var x = 0; x < shapes.length; x++) {
-    console.log(shapes[x]);
-   }
