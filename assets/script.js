71757c5f6a6bdccc836c5942a345014bd1aa1ac2
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-01-16 19:35:47 -0500

    JavaScript

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics);
+
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+}   else if (topic === 'CSS') {
+    console.log ("let's study CSS!");
+}   else if (topic === 'Git') {
+    console.log ("let's study Git!");
+}   else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+}   else {
+    console.log("Plese try again!");
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+   }
